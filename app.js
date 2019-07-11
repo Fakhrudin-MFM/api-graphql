c6464e87c4d1a2829765d66359c91d384b8bb458
millea <millea1994@gmail.com>
2019-07-11 19:35:57 +0700

    Graphql Schema and Add List APi from JSon data

--- app.js
+++ app.js
@@ -4,7 +4,29 @@ const { buildSchema } = require('graphql');
 
 const app = express()
 
+let ForumsData = [
+	{id : 1, title: "This the first Title of Forum", desc : "This the first Description from the first forum"},
+	{id : 2, title: "This the second Title of Forum", desc : "This the first Description from the second forum"},
+	{id : 3, title: "This the Third Title of Forum", desc : "This the first Description from the Third forum"}
+];
+
+let schema = buildSchema(`
+	type Forum {
+		id : ID,
+		title : String,
+		desc : String
+	}
+	type Query {
+		forums : [Forum]
+	}
+`)
+
+let resolvers = {
+	forums: ()=> ForumsData
+}
 app.use('/graphql', graphqlHTTP({
+	schema: schema,
+	rootValue: resolvers,
 	graphiql : true // To set GUI For testing
 }))
 
