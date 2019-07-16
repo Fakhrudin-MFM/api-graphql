335da9fb7b04ad8581bcb06acb760d28561cf6f2
millea <millea1994@gmail.com>
2019-07-16 22:35:34 +0700

    relations user data and forum data

--- app.js
+++ app.js
@@ -5,20 +5,34 @@ const { buildSchema } = require('graphql');
 const app = express()
 
 let ForumsData = [
-	{id : 1, title: "This the first Title of Forum", desc : "This the first Description from the first forum"},
-	{id : 2, title: "This the second Title of Forum", desc : "This the first Description from the second forum"},
-	{id : 3, title: "This the Third Title of Forum", desc : "This the first Description from the Third forum"}
+	{id : 1, title: "This the first Title of Forum", desc : "This the first Description from the first forum", userId: "1"},
+	{id : 2, title: "This the second Title of Forum", desc : "This the first Description from the second forum", userId: "2"},
+	{id : 3, title: "This the Third Title of Forum", desc : "This the first Description from the Third forum", userId: "2"}
 ];
 
+let userData = [
+	{ id : "1", name: "Millea"},
+	{ id : "2", name: "Duski"},
+	{ id : "3", name: "Laguna"}
+]
 let schema = buildSchema(`
 	type Forum {
 		id : ID,
 		title : String,
-		desc : String
+		desc : String,
+		user:User
 	}
+
+	type User {
+		id : ID,
+		name : String
+	}
+
 	type Query {
 		forum(id : ID!) : Forum,
-		forums : [Forum]
+		forums : [Forum],
+		user(id :ID!) : User,
+		users: [User]
 	}
 `)
 
@@ -26,7 +40,11 @@ let resolvers = {
 	forum: (args)=> {
 		return ForumsData.find(el => el.id == args.id)
 	},
-	forums: ()=> ForumsData
+	forums: ()=> ForumsData,
+	user: (args) => {
+		return userData.find(el => el.id == args.id)
+	},
+	users:()=> userData
 }
 app.use('/graphql', graphqlHTTP({
 	schema: schema,
